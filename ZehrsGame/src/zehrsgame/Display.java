1a02392d14e9c5c27ce577fffeb466a4de37c275
jason <jason@10.16.168.241>
2019-12-14 12:12:02 -0500

    added display jpanel to gui

---
+++ ZehrsGame/src/zehrsgame/Display.java
@@ -0,0 +1,37 @@
+package zehrsgame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+public class Display extends JPanel{
+    
+    //does the actual drawing
+     private void doDrawing(Graphics g) {        
+        //the Graphics2D class is the class that handles all the drawing
+        //must be casted from older Graphics class in order to have access to some newer methods
+        Graphics2D g2d = (Graphics2D) g;
+        //draw a string on the panel        
+        g2d.drawString("Java 2D", 50, 50); //(text, x, y)
+        g2d.drawOval(0, 0, 100, 100);  //(x,y,width,height)
+        g2d.setColor(Color.red);//change color
+        int x, y;
+        for(int i = 0; i < 1000; i++){
+            //get random x,y location
+            x =(int)(Math.random()*getWidth());
+            y =(int)(Math.random()*getHeight());
+            //draw a really short line (point)
+            g2d.drawLine(x,y,x,y);
+        }
+    }
+     
+    //overrides paintComponent in JPanel class
+    //performs custom painting
+    @Override
+    public void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);//does the necessary work to prepare the panel for drawing
+        doDrawing(g);
+    }
+}
